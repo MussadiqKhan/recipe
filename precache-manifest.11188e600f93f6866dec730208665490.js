afa5b9c1dac615364358f91867c1413734879645
MussadiqKhan <34172073+MussadiqKhan@users.noreply.github.com>
2019-08-07 23:19:38 +0500

    Updates

---
+++ precache-manifest.11188e600f93f6866dec730208665490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b20146b78383d6fab7aa40212bc5d2",
+    "url": "/recipe/index.html"
+  },
+  {
+    "revision": "bd84529a31a5c727a2a8",
+    "url": "/recipe/static/css/main.24f23294.chunk.css"
+  },
+  {
+    "revision": "e269caec8435623e6b29",
+    "url": "/recipe/static/js/2.881939cd.chunk.js"
+  },
+  {
+    "revision": "bd84529a31a5c727a2a8",
+    "url": "/recipe/static/js/main.a56f8475.chunk.js"
+  },
+  {
+    "revision": "54f1294c5f2062f68e38",
+    "url": "/recipe/static/js/runtime~main.f06815e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/recipe/static/media/logo.5d5d9eef.svg"
+  }
+]);
